422ef1246e8ed967b280bc32b6ea1bc7320e9876
Niklas Groß (inf1258) <niklas.gross@smail.th-koeln.de>
2020-09-20 18:53:20 +0200

    aulösung + video

--- project/src/main/kotlin/cga/exercise/main.kt
+++ project/src/main/kotlin/cga/exercise/main.kt
@@ -3,8 +3,9 @@ package cga.exercise
 import cga.exercise.game.Game
 
 fun main() {
-    //val game = Game(1920, 1080)
-    val game = Game(720, 640)
-
+   // val game = Game(1920, 1080)
+   // val game = Game(720, 640)
+    //val game = Game(1280, 720)
+    val game = Game(960, 880)
     game.run()
 }
